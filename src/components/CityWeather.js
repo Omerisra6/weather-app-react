97cec0ec9410aba0dba39f880d58fe81e51efc13
Omerisra06 <Omerisraeli6@gmail.com>
2023-04-19 17:10:07 +0300

    Tweak: Variable Declarstion changed

--- src/components/CityWeather.js
+++ src/components/CityWeather.js
@@ -2,50 +2,59 @@ import React from 'react'
 import Hour from './Hour'
 
 
-export default function CityWeather( { weather } ) {
+export default function CityWeather( { weather } ) 
+{
     weather = weather.data
+    const { name, country, timezone } = weather.city
+    const time                        = getHourByTimeZone( timezone ) 
+    const temp                        = toC( weather.list[ 0 ].main.temp ) 
+    const feeling                     = weather.list[ 0 ].weather[ 0 ].main
+
     return (
-        <div className={`${weather.list[0].weather[0].main} city-container`}>
+        <div className="city-container">
             <div className="details-container">
                 <div className="city-current">
 
                     <div className="city-details">
 
-                        <h1 className="city-name">{ weather.city.name}, {weather.city.country}</h1>
+                        <h1 className="city-name">{ name }, { country }</h1>
 
                         <div className="city-time">
-                            <h2 className="city-hour">{GetTimeByZone(weather.city.timezone)}</h2>
+                            <h2 className="city-hour">{ time }</h2>
                         </div>
 
-                        <div className="main-current-temp">{ToC(weather.list[0].main.temp ) }</div>
+                        <div className="main-current-temp">{ temp }</div>
                         
                         
-                        <div className="current-feeling">{ weather.list[0].weather[0].main}</div>
+                        <div className="current-feeling">{ feeling }</div>
                         
                         
                     </div>
                 </div>
 
                 <div className="hours-container">
-                    {weather.list.slice(0, 8).map(( hour, index) => {
-                        return Hour(hour)
+                    { weather.list.slice( 0, 8 ).map( ( hourWeather, index ) => {
 
-                    })}
+                        return <Hour hourWeather={ hourWeather } key={ index }/>
+                    }) }
                 </div>
             </div>
         </div>
     )
 }
 
-function ToC( kelvin ) { return ( Math.round( kelvin - 273.15)+ '\xB0C' ) }
+export function toC( kelvin ) { return ( Math.round( kelvin - 273.15)+ '\xB0C' ) }
 
-function GetTimeByZone( offset ) {
+function getHourByTimeZone( timezoneOffset ) 
+{
+    const currentTime          = Date.now();
+    const timezoneOffsetMillis = timezoneOffset * 1000;
+    const timezoneTime         = currentTime + timezoneOffsetMillis;
 
-    var d = new Date();
+    const timezoneDate   = new Date( timezoneTime );
+    const hours          = timezoneDate.getHours();
+    const minutes        = timezoneDate.getMinutes()
+    const minutesDisplay = minutes > 9 ? minutes : '0' + minutes;
 
-    var utc = d.getTime() + ( d.getTimezoneOffset() * 60000 );
-
-    var nd = new Date( utc + ( 3600000*offset ) );
-
-    return nd.toLocaleString().substring(12,17)
+    return hours + ':' + minutesDisplay;
 }
--- src/components/Hour.js
+++ src/components/Hour.js
@@ -1,39 +1,48 @@
-import { stubString } from 'lodash'
 import React from 'react'
+import { toC } from './CityWeather'
 
-export default function Hour(hourWeather) {
+export default function Hour( { hourWeather }) 
+{
+    const hourTemp = toC( hourWeather.main.temp )
+    const iconName = getIconName( hourWeather.weather[0].main )
+    const hourTime = hourWeather.dt_txt.substring( 11,16 ) 
+    const hourDate = formatDate( hourWeather.dt_txt.substring( 0,10 ) )
+    
     return (
         <div className="hour-container">
             <div className="top-hour">
-                <h2 className="hour-temp">{ToC(hourWeather.main.temp)}</h2>
+                <h2 className="hour-temp">{ hourTemp }</h2>
 
-                <h2 className="hour-feeling"><i className={`fa fa-${SetIconName(hourWeather.weather[0].main)}`}></i></h2>
+                <h2 className="hour-feeling"><i className={ `fa fa-${ iconName }` }></i></h2>
             </div>
 
-            <h2 className="hour">{hourWeather.dt_txt.substring(11,16)}</h2>
+            <div className="hour">
+                <h2 >{ hourTime }</h2>
+                <h2 className="date">{ hourDate }</h2>
+            </div>
+            
         </div>
     )
 }
 
-function ToC(kelvin) {return ( Math.round( kelvin - 273.15)+ '\xB0C.')}
-
-function SetIconName(condition) {
-    switch (condition) {
-        case 'Rain':
-            return 'cloud-rain'
-            break;
- 
-         case 'Clouds':
-         return 'cloud'
-         break;
- 
-         case 'Snow':
-             return 'snowflake'
-             break;
- 
-        default:
-            return 'sun'
-            break;
+function getIconName( condition ) 
+{
+    const iconMap = {
+        'Rain': 'cloud-rain',
+        'Clouds': 'cloud',
+        'Snow': 'snowflake',
+        'Clear': 'sun'
     }
-     
+
+    return iconMap[ condition ]
+}
+
+function formatDate( date ) 
+{   
+   
+    const fullDate  = new Date ( date )
+    const monthName = fullDate.toLocaleString( 'default', { month: 'long' } );
+    const day       = fullDate.getDate()
+
+    return `${ day } ${ monthName } `
 }
